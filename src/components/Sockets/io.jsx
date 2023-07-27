d153586baacc5871b0d93fd463ef53319c1c3742
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-27 11:51:02 +0300

    socket-client

--- src/components/Sockets/io.jsx
+++ src/components/Sockets/io.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import io from 'socket.io-client'
 const socket = io.connect('http://localhost:3002')
 // connect the socket-io to the server in the backend
@@ -9,10 +9,39 @@ const socket = io.connect('http://localhost:3002')
 function Io() {
 
 
+    // flow goes sth like this, emit a message to the backend, then, the backend receives the message, the backend then sends the received message to the frontend to be dipslayed to the user
+
+    const [message, setMessage] = useState('')
+
+    // we want to capture whatever the user types in the box and send it to the backend and back to the frontend
+
+    const handleMessage =(e)=>{
+
+        setMessage(e.target.value)
+    }
+
     const sendMessage = ()=>{
 
+        socket.emit('send_message', {message})
+
+        //the backend serves as a layer, you can only emit a message to the backend
 
     }
+
+    useEffect(()=>{
+
+        socket.on('received_message', (data)=>{
+
+            // console.log(data)
+
+            alert(data.message)
+
+
+        })
+
+
+
+    },[socket])
 
 
   return (
@@ -23,7 +52,7 @@ function Io() {
         <div className="input">
 
 
-         <input type='text' placeholder ='Enter your message'/>
+         <input type='text' onChange ={handleMessage} placeholder ='Enter your message'/>
 
          <button onClick ={sendMessage}>Send Message</button>
 
